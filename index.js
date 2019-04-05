3da708a0cc58454c52974c30a8efb93cb9dd6ec8
ANDERSON S VIEIRA <anderson.vieira-ext@viavarejo.com.br>
2019-04-05 15:38:27 -0300

    Print dos dados e inclusao de lib de csv

--- index.js
+++ index.js
@@ -4,16 +4,39 @@ console.log("Recover info for AppStore");
 
 var appleStoreId = 553834731;
 
-
 store.app({id: appleStoreId, country: 'br'}).then((data) => {
-    var id = data.id;
-    var appId = data.appId;
     console.log(`##################### App Info #####################`);
     console.log(`-> Title: ${data.title}`);
     console.log(`-> Score: ${data.score}`);
-    console.log(`-> AppId: ${appId}`);
-    console.log(`-> Id: ${id}`);
+    console.log(`-> AppId: ${data.appId}`);
+    console.log(`-> Id: ${data.id}`);
     console.log("####################################################");
+
+    console.log(`##################### Reviews #####################`);
+    
+
+    for(var i = 1; i < 10 ;i++){
+        store.reviews({
+            appId: data.appId,
+            sort: store.sort.RECENT,
+            page: i,
+            country: 'br'
+          })
+          .then((dataReview) => {
+            
+            console.log("Data length " + dataReview.length);
+            
+            dataReview.forEach(function(review) {
+                console.log("----------------------------------------------------");
+                console.log(review.score);
+                console.log(review.title);
+                console.log(review.text);
+              });
+    
+          });
+    }
+    console.log("####################################################");
+
   }).catch((error) => {
     console.debug('Failed');
   });
