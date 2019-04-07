124fcff1584679b933d37dbd55f254684678ce69
Your Name <anderson.si.vieira@hotmail.com>
2019-04-07 09:33:11 -0300

    Remoção de ponto-virgul

--- appstore.js
+++ appstore.js
@@ -1,17 +1,18 @@
-var store = require('app-store-scraper');
-const createCsvWriter = require('csv-writer').createArrayCsvWriter;
-const Logger = require('./logger');
+var store = require('app-store-scraper')
+const createCsvWriter = require('csv-writer').createArrayCsvWriter
+const Logger = require('./logger')
 
-var appleStoreId = 553834731;
+var appleStoreId = 553834731
 
 store.app({id: appleStoreId, country: 'br'}).then((data) => {
 
-    let logger = new Logger('App Store', data.title, data.score, data.appId, data.id);
-    logger.log();
+    let logger = new Logger('App Store', data.title, data.score, data.appId, data.id)
+    logger.log()
 
-    const records = [];
-
-    for(var i = 1; i < 10 ;i++){
+    const records = []
+    
+    //limit 10 pages
+    for(var i = 1; i <= 10 ;i++){
         store.reviews({
             appId: data.appId,
             sort: store.sort.RECENT,
@@ -20,22 +21,22 @@ store.app({id: appleStoreId, country: 'br'}).then((data) => {
           })
           .then((dataReview) => {
             dataReview.forEach(function(review) {
-                var reviewModel = [review.score, review.title, review.text];
-                records.push(reviewModel);
-            });
+                var reviewModel = [review.score, review.title, review.text]
+                records.push(reviewModel)
+            })
 
             const csvWriter = createCsvWriter({
                 header: ['Nota', 'Título', 'Comentário'],
                 path: 'appstore.csv'
-            });
+            })
 
             csvWriter.writeRecords(records)
             .then(() => {
-                console.log('...Done');
-            }); 
-        });
+                console.log('...Done')
+            })
+        })
     }
 
   }).catch((error) => {
-    console.debug('Failed');
-  });
+    console.debug('Failed')
+  })
